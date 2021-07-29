10992d097483cff18916527b676ff5bf9db787d4
Charles Wood <charlesctwood@gmail.com>
2021-07-28 20:47:21 -0400

    added users and products

--- app/website/models.py
+++ app/website/models.py
@@ -5,7 +5,36 @@ from flask_login import UserMixin
 def load_user(user_id):
     return User.querry.get(int(user_id))
 
+association_table = Table('association', Base.metadata,
+    Column('user_id', ForeignKey('User_Accounts.user_id')),
+    Column('product_id', ForeignKey('Products.product_id'))
+)
+
 class User(db.model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False)
-    email = db.Column(db.String(40), nullable  bv)
+    __tablename__ = 'User_Accounts'
+    user_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(40), unique=True, nullable=False)
+    password = db.Column(db.String(100), nallable=False)
+    subscriber = db.Column(db.Boolean, default=False)
+    purchases = relationship("Product", secondary=association_table)
+
+    def get_id(self):
+        return(self.user_id)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
+
+class Product(db.model):
+    __tablename__ = 'Products'
+    product_id = db.Column(db.Integer, primary_key=True)
+    product_name = db.Column(db.String(20), unique=True, nullable=False)
+    product_description = db.Column(db.String, unique=True, nullable=False)
+    product_price = db.Column(db.Integer, required=True)
+    image_file
+
+    def get_id(self):
+        return(self.product_id)
+
+    def __repr__(self):
+        return f"Product('{self.product_name}', '{self.email}')"
